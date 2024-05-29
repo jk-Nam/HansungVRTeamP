a123488ba8b466d000a8355fa1dc6d118411a6ff
jk-Nam <skawnsrn94@gmail.com>
2024-05-29 12:22:51 +0900

    Reapply "Reapply "Module inheritance definition""
    This reverts commit e5f91ad136727893f4351ec957f1ffb827fb0072.

---
+++ Team/Assets/02_Scripts/BombModule.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BombModule : MonoBehaviour
+{
+    public enum BombMoudleType
+    {
+        Wire,
+        Keypad,
+        Maze
+    }
+
+    public BombMoudleType moduleType;
+
+    protected int incorrectCnt = 0;
+    protected bool isDefused = false;
+
+    public abstract void InitiallizeModule();
+
+    public bool IsDefused()
+    {
+        return isDefused;
+    }
+
+    public abstract void DefuseModule();
+
+    public abstract void Fail();
+}
