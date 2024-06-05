e1867702d6bf68d374ce4c526e875137ba1a7a0b
xqiwipx <xqiwipx@gmail.com>
2024-06-05 18:40:34 +0900

    240605_c
    DEV내용 덮어씌움

---
+++ Team/Assets/02_Scripts/GameManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum Difficulty
+    {
+        Easy,
+        Hard
+    }
+
+    public Difficulty difficulty;
+
+    public static GameManager Instance;
+
+    public GameObject bomb;
+
+
+    public float timer = 300.0f;
+    public int defuesedCnt = 0;
+    public int incorrectCnt = 0;
+    public int totalModuleCnt;
+
+    public bool isGameStart = false;
+    public bool isGameOver = false;
+    public bool isClear = false;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        //타이머
+        if (isGameStart && (!isGameOver || isClear))
+        {
+            float dwTime = 0;
+            dwTime += Time.deltaTime;
+
+            timer -= dwTime;
+            if (timer <= 0)
+            {
+                isGameOver = true;
+                Debug.Log("폭탄이 폭발하였습니다!!!");
+                return;
+            }
+        }
+    }
+
+    void Start()
+    {
+        isGameOver = false;
+        isGameStart = false;
+    }
+
+    //난이도 설정 기본은 쉬움
+    public void SelectDifficulty()
+    {
+        switch (difficulty)
+        {
+            case Difficulty.Easy:
+                totalModuleCnt = 2;
+                break;
+            case Difficulty.Hard:
+                totalModuleCnt = 3;
+                break;
+            default:
+                totalModuleCnt = 2;
+                break;
+        }
+    }
+
+    public void GameStart()
+    {
+        isGameOver = false;
+        isGameStart = true;
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        isGameStart = false;
+        ShowResultUI();
+    }
+
+    public void GameClear()
+    {
+        isGameStart = false;
+        isClear = true;
+    }
+
+    public void ShowResultUI()
+    {
+        //게임 결과창 On
+    }
+
+}
