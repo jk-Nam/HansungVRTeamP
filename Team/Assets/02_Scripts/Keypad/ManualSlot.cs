5a589cac8668ea68d7194ab48ae822b688f3bcb5
xqiwipx <xqiwipx@gmail.com>
2024-06-10 18:46:19 +0900

    240610_b
    키패드 작업중

---
+++ Team/Assets/02_Scripts/Keypad/ManualSlot.cs
@@ -0,0 +1,55 @@
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManualSlot : MonoBehaviour
+{
+    //메뉴얼에 나온 문자열을 리스트로 저장하는 스크립트 입니다.
+    public ItemBuffer itemBuffer; //아이템 리스트
+    public Transform mRoot; //심볼이 들어갈 장소
+    private List<Slot> mSlot; //메뉴얼 심볼 리스트
+
+    public Transform ranRoot; //선택된 리스트 들어갈 장소
+    private List<Slot> ranSlot; //랜덤으로 선택된 심볼들
+
+    public Transform tRoot; //정답용 심볼 들어갈 장소
+    private List<Slot> tSlot; //정답으로 선택된 심볼들
+
+    public Transform qRoot; //문제용 버튼이미지들
+    private List<Slot> qSlot; //문제용 버튼 리스트
+
+    void Start()
+    {
+        itemBuffer = GameObject.Find("KeypadScript").GetComponent<ItemBuffer>();
+
+        mSlot = new List<Slot>(); //슬롯 리스트 정의
+        //넣어야 할 문양번호 리스트
+        List<int> numbers = new List<int>() 
+        {   28, 13, 30, 12, 7, 9, 23,
+            16, 28, 23, 26, 3, 9, 20,
+            1, 8, 26, 5, 15, 30, 3,
+            11, 21, 31, 7, 5, 20, 4,
+            24, 4, 31, 22, 21, 19, 2,
+            11, 16, 27, 14, 24, 18, 6 
+        };
+
+        //문양번호 리스트에 나온내용을 슬롯리스트에 넣는다.
+        for (int i = 0; i < mRoot.childCount; i++)
+        {
+            //대상슬롯은 i번째 슬롯의 컨포넌트 
+            var slot = mRoot.GetChild(i).GetComponent<Slot>();
+            int j = numbers[i];
+            slot.SetItem(itemBuffer.items[j]);
+
+            mSlot.Add(slot);
+        }
+
+        //6줄 중에서 랜덤으로 1줄 선택 한다
+        //선택한 줄에서 4개를 추출하여 정답을 준비한다
+        //4개를 버튼에 랜덤 삽입한다.
+        //버튼을 누르면 정답과 비교한다.
+    }
+}
---
+++ Team/Assets/02_Scripts/Keypad/Slot.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    //[HideInInspector]
+    // 해당 아이템 선언?
+    public ItemProperty item;
+    public ItemBuffer itemBuffer; //아이템 리스트
+
+    public Image image; // 변하는 이미지 선언
+
+
+    //아이템 장착 함수 정의 (아이템 정보)
+    public void SetItem(ItemProperty item)
+    {
+        //현재 아이템에 아이템 정보를 넣는다
+        this.item = item;
+
+        //Debug.Log(item + " 값으로 처리");
+
+        //아이템값이 널인가?
+        if (item == null || item.name == "Empty")
+        {
+            //이미지 사용을 비활성화
+            image.enabled = false;
+            //아이템이 없으면 "Empty" 넣는다
+            gameObject.name = "Empty";
+        }
+        else //아이템값이 있으면
+        {
+            //이미지 사용을 활성화
+            image.enabled = true; 
+
+            //해당 개제 이름에 아이템 이름을 넣는다
+            gameObject.name = item.name;
+            //이미지에 아이템 이미지를 넣는다
+            image.sprite = item.sprite;
+
+            
+        }
+
+    }
+
+    //판매버튼 함수를 정의한다
+    public void OnClickSellBtn()
+    {
+        //아이템 넣는 함수에 널값을 넣는다
+        //SetItem(null);
+
+        //아이템에 빈값설정 아이템을 넣는다
+        SetItem(itemBuffer.items[0]);
+    }
+}
