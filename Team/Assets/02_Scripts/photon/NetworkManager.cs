4bb0bbc45528c419313a10935b1219b9536282d1
jk-Nam <skawnsrn94@gmail.com>
2024-06-18 17:59:56 +0900

    DEV3

--- Team/Assets/02_Scripts/photon/NetworkManager.cs
+++ Team/Assets/02_Scripts/photon/NetworkManager.cs
@@ -37,14 +37,14 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     
   
 
-    //���� ��2 Ȱ���ϱ�~ ��
-    //RPC�� Ȱ�� �ؼ� ��ź�� ��ü �ƴ���, ���� �ƴ����� �˷��ֱ⢾
-    //���� ���� �� �� ������ ���� ����(������ ���ڷ� �����) ��ư�� ���ܵΰ� ���� ������ ������ �� ������ 
+    //포톤 펀2 활용하기~ ♠
+    //RPC를 활용 해서 폭탄이 해체 됐는지, 폭파 됐는지를 알려주기♥
+    //서버 접속 후 방 생성은 빠른 입장(랜덤한 숫자로 방생성) 버튼만 남겨두고 서버 만들기는 버리는 게 좋겠음 
 
   
 
-    #region �渮��Ʈ ����
-    // ����ư -2 , ����ư -1 , �� ����
+    #region 방리스트 갱신
+    // ◀버튼 -2 , ▶버튼 -1 , 셀 숫자
     public void MyListClick(int num)
     {
         if (num == -2) --currentPage;
@@ -55,14 +55,14 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     void MyListRenewal()
     {
-        // �ִ�������
+        // 최대페이지
         maxPage = (myList.Count % CellBtn.Length == 0) ? myList.Count / CellBtn.Length : myList.Count / CellBtn.Length + 1;
 
-        // ����, ������ư
+        // 이전, 다음버튼
         PreviousBtn.interactable = (currentPage <= 1) ? false : true;
         NextBtn.interactable = (currentPage >= maxPage) ? false : true;
 
-        // �������� �´� ����Ʈ ����
+        // 페이지에 맞는 리스트 대입
         multiple = (currentPage - 1) * CellBtn.Length;
         for (int i = 0; i < CellBtn.Length; i++)
         {
@@ -92,13 +92,13 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     #endregion
 
 
-    #region ��������
-    void Awake() => Screen.SetResolution(960, 540, false);
+    #region 서버연결
+    //void Awake() => Screen.SetResolution(960, 540, false);
 
     void Update()
     {
         StatusText.text = PhotonNetwork.NetworkClientState.ToString();
-        LobbyInfoText.text = (PhotonNetwork.CountOfPlayers - PhotonNetwork.CountOfPlayersInRooms) + "�κ� / " + PhotonNetwork.CountOfPlayers + "����";
+        LobbyInfoText.text = (PhotonNetwork.CountOfPlayers - PhotonNetwork.CountOfPlayersInRooms) + "로비 / " + PhotonNetwork.CountOfPlayers + "접속";
     }
 
     public void Connect() => PhotonNetwork.ConnectUsingSettings();
@@ -110,7 +110,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         LobbyPanel.SetActive(true);
         RoomPanel.SetActive(false);
         PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
-        WelcomeText.text = PhotonNetwork.LocalPlayer.NickName + "�� ȯ���մϴ�";
+        WelcomeText.text = PhotonNetwork.LocalPlayer.NickName + "님 환영합니다";
         myList.Clear();
     }
 
@@ -124,7 +124,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     #endregion
 
 
-    #region ��
+    #region 방
     public void CreateRoom() => PhotonNetwork.CreateRoom(RoomInput.text == "" ? "Room" + Random.Range(0, 100) : RoomInput.text, new RoomOptions { MaxPlayers = 2 });
 
     public void JoinRandomRoom() => PhotonNetwork.JoinRandomRoom();
@@ -146,7 +146,6 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         RoomRenewal();
-
         //  ChatRPC("<color=yellow>" + newPlayer.NickName + "님이 참가하셨습니다</color>");
         UpdateStartButton();
     }
@@ -154,10 +153,8 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public override void OnPlayerLeftRoom(Player otherPlayer)
     {
         RoomRenewal();
-
         // ChatRPC("<color=yellow>" + otherPlayer.NickName + "님이 퇴장하셨습니다</color>");
         UpdateStartButton();
-
     }
 
     void RoomRenewal()
@@ -165,7 +162,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         ListText.text = "";
         for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
             ListText.text += PhotonNetwork.PlayerList[i].NickName + ((i + 1 == PhotonNetwork.PlayerList.Length) ? "" : ", ");
-        RoomInfoText.text = PhotonNetwork.CurrentRoom.Name + " / " + PhotonNetwork.CurrentRoom.PlayerCount + "�� / " + PhotonNetwork.CurrentRoom.MaxPlayers + "�ִ�";
+        RoomInfoText.text = PhotonNetwork.CurrentRoom.Name + " / " + PhotonNetwork.CurrentRoom.PlayerCount + "명 / " + PhotonNetwork.CurrentRoom.MaxPlayers + "최대";
     }
 
     //플레이어가 2명 이상 일때 스타트 버튼이 활성화
@@ -187,28 +184,43 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     #endregion
 
 
-    #region ä��
-    public void Send()
-    {
-        PV.RPC("ChatRPC", RpcTarget.All, PhotonNetwork.NickName + " : " + ChatInput.text);
-        ChatInput.text = "";
-    }
+    #region RPC
+    //public void Send()
+    //{
+    //    PV.RPC("ChatRPC", RpcTarget.All, PhotonNetwork.NickName + " : " + ChatInput.text);
+    //    ChatInput.text = "";
+    //}
 
-    [PunRPC] // RPC�� �÷��̾ �����ִ� �� ��� �ο����� �����Ѵ�
-    void ChatRPC(string msg)
+    //[punrpc] // rpc는 플레이어가 속해있는 방 모든 인원에게 전달한다
+    //void chatrpc(string msg)
+    //{
+    //    bool isinput = false;
+    //    for (int i = 0; i < chattext.length; i++)
+    //        if (chattext[i].text == "")
+    //        {
+    //            isinput = true;
+    //            chattext[i].text = msg;
+    //            break;
+    //        }
+    //    if (!isinput) // 꽉차면 한칸씩 위로 올림
+    //    {
+    //        for (int i = 1; i < chattext.length; i++) chattext[i - 1].text = chattext[i].text;
+    //        chattext[chattext.length - 1].text = msg;
+    //    }
+    //}
+
+    [PunRPC] 
+    void GameOverRPC()
     {
-        bool isInput = false;
-        for (int i = 0; i < ChatText.Length; i++)
-            if (ChatText[i].text == "")
-            {
-                isInput = true;
-                ChatText[i].text = msg;
-                break;
-            }
-        if (!isInput) // ������ ��ĭ�� ���� �ø�
+        // GameManager 스크립트의 GameOver 호출
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        if (gameManager != null)
         {
-            for (int i = 1; i < ChatText.Length; i++) ChatText[i - 1].text = ChatText[i].text;
-            ChatText[ChatText.Length - 1].text = msg;
+            gameManager.GameOver();
+        }
+        else
+        {
+            Debug.LogError("GameManager 스크립트를 찾을 수 없습니다.");
         }
     }
     #endregion
