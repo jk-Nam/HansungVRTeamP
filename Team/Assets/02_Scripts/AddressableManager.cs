7f2e0b81cfb8b6ea593400342ae473ddf1a4e8a4
박준영 <tenzingberguy@gmail.com>
2024-06-25 17:05:17 +0900

    test

--- Team/Assets/02_Scripts/AddressableManager.cs
+++ Team/Assets/02_Scripts/AddressableManager.cs
@@ -7,27 +7,124 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AddressableManager : MonoBehaviour
 {
+    public static AddressableManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    AsyncOperationHandle<long> bombHandle;
+    AsyncOperationHandle<long> MatHandle;
+    AsyncOperationHandle<long> wireHandle;
+    AsyncOperationHandle<long> brokenWireHandle;
+    AsyncOperationHandle<long> shaderHandle;
+
+    public Transform bombPos;
+
+    public AssetReference bombPrefab;
     public AssetReference wirePrefab;
 
-    public void DownloadAsset(object key)
+    public string LableForBundleDown;
+
+    private List<string> matKeys = new List<string>
     {
-        Addressables.GetDownloadSizeAsync(key).Completed += (opSize) =>
+        "Assets/08_Materials/Red.mat",
+        "Assets/08_Materials/Blue.mat",
+        "Assets/08_Materials/Gray.mat",
+        "Assets/08_Materials/White.mat",
+        "Assets/08_Materials/Yellow.mat",
+        "Assets/08_Materials/Black.mat"
+    };
+
+    void Start()
+    {
+        Addressables.ClearDependencyCacheAsync("Bomb");
+        Addressables.ClearDependencyCacheAsync("Mat");
+        Addressables.ClearDependencyCacheAsync("Wire");
+        Addressables.ClearDependencyCacheAsync("Shader");
+
+        StartCoroutine(CheckDownLoadFileSize());
+
+        //CreateBomb();
+    }
+
+    private void OnClearCacheCompleted(AsyncOperationHandle<IList<object>> handle)
+    {
+        if (handle.Status == AsyncOperationStatus.Succeeded)
         {
-            if (opSize.Status == AsyncOperationStatus.Succeeded && opSize.Result > 0)
+            Debug.Log("Cache cleared successfully for label: " + handle.Result);
+        }
+        else
+        {
+            Debug.LogError("Failed to clear cache for label: " + handle.Result);
+        }
+
+        // 핸들 해제
+        Addressables.Release(handle);
+    }
+
+    IEnumerator CheckDownLoadFileSize()
+    {
+        Debug.Log("CheckDownLoadFileSize!!!");
+
+        bombHandle = Addressables.GetDownloadSizeAsync("Bomb");
+        yield return bombHandle;
+        MatHandle = Addressables.GetDownloadSizeAsync("Mat");
+        yield return MatHandle;
+        wireHandle = Addressables.GetDownloadSizeAsync("Wire");
+        yield return wireHandle;
+        brokenWireHandle = Addressables.GetDownloadSizeAsync("BrokenWire");
+        yield return brokenWireHandle;
+        shaderHandle = Addressables.GetDownloadSizeAsync("Shader");
+        yield return shaderHandle;
+
+        Debug.Log(MatHandle.Result + " bytes");
+
+        if (bombHandle.Result == 0 && MatHandle.Result == 0 && wireHandle.Result == 0 & shaderHandle.Result == 0)
+        {
+            Debug.Log("There is no Patch File...");
+            //패치가 없다면 에셋 로드
+            LoadAsset("Bomb");
+            foreach (var key in matKeys)
             {
-                Addressables.DownloadDependenciesAsync(key, true).Completed += (opDownload) =>
-                {
-                    if (((AsyncOperationHandle)opDownload).Status != AsyncOperationStatus.Succeeded)
-                    {
-                        return;
-                    }
-                };
+                LoadAsset(key);
             }
-            else
+            LoadAsset("Wire");
+            LoadAsset("BrokenWire");
+            LoadAsset("Assets/SimpleToon/Shader/ToonLightBase.shader");
+        }
+        else
+        {
+            Debug.Log("Patch File Found: " + MatHandle.Result + " bytes");
+            DownloadAsset();
+        }
+    }
+
+    public void DownloadAsset()
+    {
+        Addressables.DownloadDependenciesAsync(LableForBundleDown).Completed += (AsyncOperationHandle handle) =>
+        {
+            Debug.Log("다운로드 완료");
+            Addressables.Release(handle);
+            // 다운로드 완료 후 에셋 로드
+            LoadAsset("Shader");
+            LoadAsset("Bomb");
+            foreach (var key in matKeys)
             {
-                //다운로드 받을 자료 없음
-                Debug.Log("다운로드할 파일이 없습니다.");
+                LoadAsset(key);
             }
+            LoadAsset("Wire");
+            LoadAsset("BrokenWire");
         };
     }
 
@@ -35,24 +132,102 @@ public class AddressableManager : MonoBehaviour
     {
         try
         {
-            Addressables.LoadAssetAsync<GameObject>(key).Completed += (op) =>
+            if (key.ToString().EndsWith(".shader"))
             {
-                if (((AsyncOperationHandle)op).Status == AsyncOperationStatus.Succeeded)
+                Addressables.LoadAssetAsync<Shader>(key).Completed += (op) =>
                 {
-                    Debug.Log("에셋 로드 완료");
-                    return;
-                }
-            };
+                    if (op.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        Shader shader = op.Result;
+                        if (shader != null)
+                        {
+                            Debug.Log(key + ": 에셋 로드 완료, Shader: " + shader.name);
+                            //Renderer renderer = GetComponent<Renderer>();
+                            //if (renderer != null)
+                            //{
+                            //    renderer.material.shader = shader;
+                            //}
+                        }
+                        else
+                        {
+                            Debug.LogError(key + ": 로드된 Shader null입니다.");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError(key + ": 에셋 로드 실패");
+                    }
+                };
+            }
+            else if (key.ToString().EndsWith(".mat")) //메테리얼 에셋 처리
+            {
+                Addressables.LoadAssetAsync<Material>(key).Completed += (op) =>
+                {
+                    if (op.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        Material material = op.Result;
+                        if (material != null)
+                        {
+                            Debug.Log(key + ": 에셋 로드 완료, Material: " + material.name);
+                        }
+                        else
+                        {
+                            Debug.LogError(key + ": 로드된 Material이 null입니다.");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError(key + ": 에셋 로드 실패");
+                    }
+                };
+            }
+            else // 메테리얼 이외의 에셋 처리
+            {
+                Addressables.LoadAssetAsync<GameObject>(key).Completed += (op) =>
+                {
+                    if (op.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        GameObject loadedAsset = op.Result;
+                        if (loadedAsset != null)
+                        {
+                            Debug.Log(key + ": 에셋 로드 완료, GameObject: " + loadedAsset.name);
+                        }
+                        else
+                        {
+                            Debug.LogError(key + ": 로드된 GameObject가 null입니다.");
+                        }
+                    }
+                };
+            }
         }
-        catch(Exception e) { Debug.LogError(e.Message); }
+        catch (Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
     }
 
-    public void CreatePrefab()
+
+    //폭탄 생성 *게임 매니저로 옮길 가능성 있음
+    public void CreateBomb()
     {
         List<AsyncOperationHandle<GameObject>> handles = new List<AsyncOperationHandle<GameObject>>();
 
-        AsyncOperationHandle<GameObject> handle = wirePrefab.InstantiateAsync();
-        handles.Add(handle);
-    }
+        AsyncOperationHandle<GameObject> handle = bombPrefab.InstantiateAsync();
 
+        handles.Add(handle);
+
+        handle.Completed += (AsyncOperationHandle<GameObject> completeHandle) =>
+        {
+            GameObject bomb = completeHandle.Result;
+            if (bomb != null)
+            {
+                bomb.transform.position = bombPos.transform.position;
+                Debug.Log("폭탄 생성 완료: " + bomb.name);
+            }
+            else
+            {
+                Debug.LogError("폭탄 생성 실패: 로드된 GameObject가 null입니다.");
+            }
+        };
+    }
 }
--- Team/Assets/02_Scripts/WiresModule.cs
+++ Team/Assets/02_Scripts/WiresModule.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class WiresModule : BombModule
 {
@@ -10,17 +12,26 @@ public class WiresModule : BombModule
     }
 
     Bomb bomb;
+    CutTest cut;
 
-    public List<string> wireColor = new List<string> { "Red", "Blue", "Black", "White", "Yellow"};
+    public AssetReference wirePrefab;
+    public AssetReference brokenWirePrefab;
+    public Transform[] wirePos;
 
-    int wireCnt;
+    public List<string> wireColor = new List<string> { "Red", "Blue", "Black", "White", "Yellow" };
+    public List<Material> mats;
+
     public int correctWireNum;
+    int wireCnt;
+
+    public List<GameObject> wires = new List<GameObject>();
+    public List<GameObject> brokenWires = new List<GameObject>();
 
     private void Awake()
     {
         bomb = GameObject.FindGameObjectWithTag("BOMB").GetComponent<Bomb>();
+        cut = GetComponentInParent<CutTest>();
     }
-
 
     private void Start()
     {
@@ -28,20 +39,20 @@ public class WiresModule : BombModule
         DefuseModule();
     }
 
+    //모듈 초기화
     public override void InitiallizeModule()
     {
         GameManager.Instance.defuesedCnt = 0;
         GameManager.Instance.incorrectCnt = 0;
         wireCnt = Random.Range(3, 6);
-        //wireCnt = 5;
-        Debug.Log("Wire�� ���� " + wireCnt + "�� �Դϴ�.");
+        Debug.Log("Wire의 수는 " + wireCnt + "개 입니다.");
     }
 
     public override void DefuseModule()
     {
         switch (wireCnt)
         {
-            case 3:
+            case 3: //와이어의 갯수가 3개일 경우
                 List<string> ThreeColor = GetRandomColors(wireColor, 3);
                 foreach (string color in ThreeColor)
                 {
@@ -53,38 +64,39 @@ public class WiresModule : BombModule
                     if (wireColor[2] == "White")
                     {
                         correctWireNum = 3;
-                        Debug.Log("1-1 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                        Debug.Log("1-1 잘라야 하는 와이어 : " + correctWireNum + "번");
                     }
                     else
                     {
                         if (wireColor.FindAll(w => w == "Blue").Count >= 2)
                         {
-                            //�� �� ��, �� �� ��, �� �� ��
+                            //빨 파 파, 파 빨 파, 파 파 빨
                             if (wireColor[2] == "Blue")
                             {
                                 correctWireNum = 3;
-                                Debug.Log("1-2 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                                Debug.Log("1-2 잘라야 하는 와이어 : " + correctWireNum + "번");
                             }
                             else
                             {
                                 correctWireNum = 2;
-                                Debug.Log("1-3 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                                Debug.Log("1-3 잘라야 하는 와이어 : " + correctWireNum + "번");
                             }
                         }
                         else
                         {
                             correctWireNum = 3;
-                            Debug.Log("1-4 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                            Debug.Log("1-4 잘라야 하는 와이어 : " + correctWireNum + "번");
                         }
                     }
                 }
                 else
                 {
                     correctWireNum = 2;
-                    Debug.Log("1-5 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                    Debug.Log("1-5 잘라야 하는 와이어 : " + correctWireNum + "번");
                 }
-                    break;
-            case 4:
+                break;
+
+            case 4: //와이어의 갯수가 4개일 경우
                 List<string> fourColor = GetRandomColors(wireColor, 4);
                 foreach (string color in fourColor)
                 {
@@ -94,36 +106,40 @@ public class WiresModule : BombModule
                 if (wireColor.FindAll(w => w == "Red").Count >= 2)
                 {
                     correctWireNum = wireColor.FindLastIndex(str => str == "Red") + 1;
-                    Debug.Log("4-1 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                    Debug.Log("4-1 잘라야 하는 와이어 : " + correctWireNum + "번");
                 }
                 else
                 {
                     if (wireColor[3] == "Yellow" && !wireColor.Contains("Red"))
                     {
                         correctWireNum = 1;
-                        Debug.Log("4-2 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                        Debug.Log("4-2 잘라야 하는 와이어 : " + correctWireNum + "번");
                     }
                     else
                     {
-                        if(wireColor.FindAll(w => w == "Blue").Count == 1)
+                        if (wireColor.FindAll(w => w == "Blue").Count == 1)
                         {
                             correctWireNum = 1;
-                            Debug.Log("4-3 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                            Debug.Log("4-3 잘라야 하는 와이어 : " + correctWireNum + "번");
                         }
                         else
                         {
                             if (wireColor.FindAll(w => w == "Yellow").Count >= 2)
                             {
                                 correctWireNum = 4;
-                                Debug.Log("4-4 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                                Debug.Log("4-4 잘라야 하는 와이어 : " + correctWireNum + "번");
                             }
-                            correctWireNum = 2;
-                            Debug.Log("4-5 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                            else
+                            {
+                                correctWireNum = 2;
+                                Debug.Log("4-5 잘라야 하는 와이어 : " + correctWireNum + "번");
+                            }
                         }
                     }
                 }
                 break;
-            case 5:
+
+            case 5: //와이어의 갯수가 5개일 경우
                 List<string> fiveColor = GetRandomColors(wireColor, 5);
                 foreach (string color in fiveColor)
                 {
@@ -133,56 +149,168 @@ public class WiresModule : BombModule
                 if (wireColor[4] == "Black")
                 {
                     correctWireNum = 4;
-                    Debug.Log("5-1 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                    Debug.Log("5-1 잘라야 하는 와이어 : " + correctWireNum + "번");
                 }
                 else
                 {
                     if (wireColor.FindAll(w => w == "Red").Count == 1 && wireColor.FindAll(w => w == "Yellow").Count >= 2)
                     {
                         correctWireNum = 1;
-                        Debug.Log("5-2 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                        Debug.Log("5-2 잘라야 하는 와이어 : " + correctWireNum + "번");
                     }
                     else
                     {
                         if (!wireColor.Contains("Black"))
                         {
                             correctWireNum = 2;
-                            Debug.Log("5-3 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                            Debug.Log("5-3 잘라야 하는 와이어 : " + correctWireNum + "번");
                         }
                         else
                         {
                             correctWireNum = 1;
-                            Debug.Log("5-4 �߶�� �ϴ� ���̾� : " + correctWireNum + "��");
+                            Debug.Log("5-4 잘라야 하는 와이어 : " + correctWireNum + "번");
                         }
                     }
                 }
                 break;
         }
-        
     }
 
     List<string> GetRandomColors(List<string> colors, int count)
     {
         List<string> rColors = new List<string>();
 
+        // 색깔과 Addressable 매테리얼 키 매칭
+        Dictionary<string, string> colorToMaterialAddress = new Dictionary<string, string>
+    {
+        { "Red",    "Assets/08_Materials/Red.mat" },
+        { "Blue",   "Assets/08_Materials/Blue.mat" },
+        { "Black",  "Assets/08_Materials/Black.mat" },
+        { "White",  "Assets/08_Materials/White.mat" },
+        { "Yellow", "Assets/08_Materials/Yellow.mat" }
+    };
+
+        // wirePos의 길이가 count보다 작은 경우 예외 처리
+        if (wirePos.Length < count)
+        {
+            Debug.LogError("wirePos 배열의 길이가 count보다 작습니다.");
+            return rColors;
+        }
+
         for (int i = 0; i < count; i++)
         {
             int rIdx = Random.Range(0, colors.Count);
             rColors.Add(colors[rIdx]);
-        }
 
+            // 비동기 처리를 위한 로컬 변수로 저장
+            int localIndex = i;
+            string selectedColor = colors[rIdx];
+
+            // 와이어 생성
+            AsyncOperationHandle<GameObject> handle = wirePrefab.InstantiateAsync();
+            
+
+            handle.Completed += (AsyncOperationHandle<GameObject> completeHandle) =>
+            {
+                GameObject go_Wire = completeHandle.Result;
+                wires.Add(go_Wire);
+                Transform wireTransform = wirePos[localIndex];
+                wireTransform.gameObject.SetActive(true);
+                go_Wire.transform.SetParent(wireTransform); // 로컬 변수 사용
+                go_Wire.transform.position = wireTransform.position;
+                go_Wire.transform.rotation = wireTransform.rotation;
+                go_Wire.transform.Rotate(0, 180.0f, 0, Space.Self);
+                go_Wire.transform.localScale = new Vector3(0.25f, 0.2f, 0.2f);
+
+                // 와이어 매테리얼 변경
+                if (colorToMaterialAddress.ContainsKey(selectedColor))
+                {
+                    string materialAddress = colorToMaterialAddress[selectedColor];
+                    Debug.Log("Loading material: " + materialAddress);
+                    Addressables.LoadAssetAsync<Material>(materialAddress).Completed += (AsyncOperationHandle<Material> matHandle) =>
+                    {
+                        if (matHandle.Status == AsyncOperationStatus.Succeeded)
+                        {
+                            Material loadedMaterial = matHandle.Result;
+                            Renderer wireRenderer = go_Wire.GetComponentInChildren<Renderer>();
+                            if (wireRenderer != null)
+                            {
+                                wireRenderer.material = loadedMaterial;
+                            }
+                        }
+                        else
+                        {
+                            Debug.LogError("Failed to load material: " + materialAddress);
+                        }
+                    };
+                }
+                else
+                {
+                    Debug.LogError("Color not found in dictionary: " + selectedColor);
+                }
+
+                // 절단된 와이어 생성
+                AsyncOperationHandle<GameObject> handle2 = brokenWirePrefab.InstantiateAsync();
+
+                handle2.Completed += (AsyncOperationHandle<GameObject> completeHandle) =>
+                {
+                    GameObject go_BrokenWire = completeHandle.Result;
+                    brokenWires.Add(go_BrokenWire);
+                    Transform wireTransform = wirePos[localIndex];
+                    go_BrokenWire.gameObject.SetActive(false);
+                    go_BrokenWire.transform.SetParent(wireTransform); // 로컬 변수 사용
+                    go_BrokenWire.transform.position = wireTransform.position;
+                    go_BrokenWire.transform.rotation = wireTransform.rotation;
+                    go_BrokenWire.transform.Rotate(0, 180.0f, 0, Space.Self);
+                    go_BrokenWire.transform.localScale = new Vector3(0.25f, 0.2f, 0.2f);
+
+                    // 와이어 매테리얼 변경
+                    if (colorToMaterialAddress.ContainsKey(selectedColor))
+                    {
+                        string materialAddress = colorToMaterialAddress[selectedColor];
+                        Debug.Log("Loading material: " + materialAddress);
+                        Addressables.LoadAssetAsync<Material>(materialAddress).Completed += (AsyncOperationHandle<Material> matHandle) =>
+                        {
+                            if (matHandle.Status == AsyncOperationStatus.Succeeded)
+                            {
+                                Material loadedMaterial = matHandle.Result;
+                                Renderer wireRenderer = go_BrokenWire.GetComponentInChildren<Renderer>();
+                                if (wireRenderer != null)
+                                {
+                                    wireRenderer.material = loadedMaterial;
+                                }
+                            }
+                            else
+                            {
+                                Debug.LogError("Failed to load material: " + materialAddress);
+                            }
+                        };
+                    }
+                    else
+                    {
+                        Debug.LogError("Color not found in dictionary: " + selectedColor);
+                    }
+                };
+            };
+            
+        }
+        cut.OnClickSetUp();
         return rColors;
     }
-    
+
     public void CutWire(int idx)
     {
         if (!isDefused && GameManager.Instance.isGameStart && !GameManager.Instance.isGameOver)
         {
-            if (idx == GameManager.Instance.incorrectCnt)
+            //프리팹 변경
+            wires[idx].SetActive(false);
+            brokenWires[idx].SetActive(true);
+
+            if (idx == correctWireNum)
             {
                 isDefused = true;
                 GameManager.Instance.defuesedCnt++;
-                Debug.Log("���̾� ��� ���� ����");
+                Debug.Log("와이어 모듈 해제 성공");
                 if (GameManager.Instance.defuesedCnt == GameManager.Instance.totalModuleCnt)
                 {
                     GameManager.Instance.GameClear();
@@ -191,13 +319,13 @@ public class WiresModule : BombModule
             else
             {
                 GameManager.Instance.incorrectCnt++;
-                Debug.Log("���� �� : " + GameManager.Instance.incorrectCnt);
+                Debug.Log("오답 수 : " + GameManager.Instance.incorrectCnt);
                 if (GameManager.Instance.incorrectCnt >= 3)
                 {
                     bomb.Fail();
                     Debug.Log("Game Over!!!");
                 }
             }
-        }        
+        }
     }
 }
