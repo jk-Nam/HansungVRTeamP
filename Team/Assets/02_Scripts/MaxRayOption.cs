c0163f02de18309fba506ae3fecf40b4a7b6e32c
xqiwipx <xqiwipx@gmail.com>
2024-06-26 13:41:21 +0900

    240626_b
    병합준비

--- Team/Assets/02_Scripts/MaxRayOption.cs
+++ Team/Assets/02_Scripts/MaxRayOption.cs
@@ -6,17 +6,22 @@ using Oculus.Interaction;
 
 public class MaxRayOption : MonoBehaviour
 {
-    public RayInteractor leftRay;
-    public RayInteractor rightRay;
+    //이 코드는 SceneMgr의 부족한 부분을 보완하기 위한 코드입니다.
 
-    public float rayDefault = 0.1f;
-    public float rayMax = 20.0f;
 
+    public RayInteractor leftRay; //왼손
+    public RayInteractor rightRay; //오른손
+
+    public float rayDefault = 0.1f; //초기화 값
+    public float rayMax = 20.0f; //연장값
+
+    //Ray 길이 초기화
     public void RayDefault()
     {
         leftRay.MaxRayLength = rightRay.MaxRayLength = rayDefault;
     }
 
+    //Ray 길이 연장
     public void RayMax()
     {
         leftRay.MaxRayLength = rightRay.MaxRayLength = rayMax;
